d8c9ed9aad1d117a8adfaefa2e77179d3ff8ba50
sanya <sanyapen02@gmail.com>
2024-07-22 13:21:46 +0300

    Initiate Method(nor pretty:()

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,23 +1,39 @@
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class GridManager : MonoBehaviour
 {
     public List<GameObject> Cells = new List<GameObject>();
     public List<Image> cellsImage = new List<Image>();
-    private List<GameObject> UnitPlace = new List<GameObject>();
-
+    private List<GameObject> UnitPlaces = new List<GameObject>();
+    private List<GameObject> AttackPlaces = new List<GameObject>();
+    private List<GameObject> HPPlaces = new List<GameObject>();
+    private List<GameObject> MnvrPlaces = new List<GameObject>();
+    private List<GameObject> InitPlaces = new List<GameObject>();
+    private List<TMP_Text> attackValue = new List<TMP_Text>();
+    private List<TMP_Text> HPValue = new List<TMP_Text>();
+    private List<TMP_Text> InitValue = new List<TMP_Text>();
+    private List<TMP_Text> MnvrValue = new List<TMP_Text>();
     void Start()
     {
-        CollectCellsByNames(CellsNames.Cells);
-        GetChild(Cells, "UnitPlace", UnitPlace);
-        GetImage(Cells, cellsImage);
-        Debug.Log(cellsImage.Count);
+        InitiateCells();        
     }
-
+    void InitiateCells()
+    {
+        CollectCellsByNames(CellsNames.Cells);
+        GetChild(Cells, "UnitPlace", UnitPlaces);
+        GetChild(Cells, "AttackPlace", AttackPlaces);
+        GetChild(Cells, "InitPlace", InitPlaces);
+        GetChild(Cells, "HPPlace", HPPlaces);
+        GetChild(Cells, "MnvrPlace", MnvrPlaces);
+        GetText(AttackPlaces, attackValue, "AttackValue");
+        GetText(InitPlaces, InitValue, "InitValue");
+        GetText(HPPlaces, HPValue, "HPValue");
+        GetText(MnvrPlaces, MnvrValue, "MnvrValue");
+    }
     void CollectCellsByNames(List<string> names)
     { 
         foreach (string name in names)
@@ -41,8 +57,20 @@ public class GridManager : MonoBehaviour
         foreach(GameObject gameObject in gameObjectsList)
         {
             Image image = gameObject.GetComponent<Image>();
-            image.color = Color.red;
             imagePlaceList.Add(image);
+        }
+    }
+    void GetText(List<GameObject> objectWithText,List<TMP_Text> textlist,string childName)
+    {
+        foreach (GameObject obj in objectWithText)
+        {
+            Transform parent = obj.transform;
+            Transform child = parent.Find(childName);
+            GameObject Child = child.gameObject;
+
+            TMP_Text text = Child.GetComponent<TMP_Text>();
+            
+            textlist.Add(text);
         }
     }
 }
